52c93feb1532b7f52c139deec9073d8a52e68252
JungKiHyun <jeongkyoni@gmail.com>
2024-06-10 06:51:36 +0900

    note images추가 지판 음 배열 랜덤 출력

--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -1,65 +1,56 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Note.scss'
 
 const Note = ({openModal}) => {
-    const [selectedNote, setSelectedNote] = useState(null);
-  const [answerOptions, setAnswerOptions] = useState([]);
-  const [correctNote, setCorrectNote] = useState('');
   const [result, setResult] = useState('');
-  const [pointPosition, setPointPosition] = useState({ top: '0px', left: '0px' });
-  const pointPositionArr = [
-    {top: 48, left: 80},
-    {top: 75, left: 80},
-    {top: 101, left: 80},
-    {top: 127, left: 80},
-    {top: 155, left: 80},
-    {top: 184, left: 80},
-  ]
 
-  const notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+  const [notes, setNotes] = useState(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'G♯/A♭', 'D♯/E♭', 'A♯/B♭', 'F♯/G♭', 'C♯/D♭']);
+  const guitarRef = useRef(null);
+
+  const [fret, setFret] = useState(0)
+  const [line, setLine] = useState(0)
+  const [count, setCount] = useState(1)
+  const [noteButtons, setNoteButtons] = useState()
 
   useEffect(() => {
-    const randomNote = notes[Math.floor(Math.random() * notes.length)];
-    setCorrectNote(randomNote);
+    let fretRandom = Math.floor(Math.random() * 12) + 1
+    let lineRandom = Math.floor(Math.random()* 6) + 1
+    setFret(fretRandom)
+    setLine(lineRandom)
+    suffleNotes()
+  }, [count])
 
-    const shuffledNotes = notes.sort(() => 0.5 - Math.random());
-    const options = shuffledNotes.slice(0, 3);
-    options.push(randomNote);
-    setAnswerOptions(options.sort(() => 0.5 - Math.random()));
-
-    // 랜덤 위치 설정
-    let index = 5;
-    const top = pointPositionArr[index].top
-    const left = pointPositionArr[index].left
-    // const top = `${Math.floor(Math.random() * 80) + 10}%`; // 10% ~ 90% 사이의 값
-    // const left = `${Math.floor(Math.random() * 80) + 10}%`; // 10% ~ 90% 사이의 값
-    
-    setPointPosition({ top, left });
-  }, []);
+  const suffleNotes = () => {
+    let newArray = [...notes];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    setNotes(newArray)
+    // return newArray;
+  }
 
   const handleNoteClick = (note) => {
-    setSelectedNote(note);
-    if (note === correctNote) {
-      setResult('정답입니다!');
-    } else {
-      setResult('오답입니다. 다시 시도하세요.');
-    }
+    // setSelectedNote(note);
+    // if (note === correctNote) {
+    //   openModal('', '정답입니다!', () => {});
+    // } else {
+    //   openModal('', '댕!', () => {});
+    // }
+      // openModal('', '정답입니다!', () => {setCount(++count)});
+    const addCount = count + 1
+    setCount(addCount)
   };
 
   return (
     <div className="note-container">
-      <div className="guitar-image">
-        <img src="/img/note1-3.png" alt="Guitar" />
-        <img
-          src="/img/point.png"
-          alt="Point"
-          className="point-image"
-          style={{ top: pointPosition.top, left: pointPosition.left }}
-        />
+      {count}
+      <div className="guitar-image" ref={guitarRef}>
+        <img src={`/img/notes/note${fret}${line}.png`} alt="Guitar" />
       </div>
       <div className="answer-options">
-        {answerOptions.map((note, index) => (
+        {notes.map((note, index) => (
           <button key={index} onClick={() => handleNoteClick(note)}>
             {note}
           </button>
@@ -70,4 +61,4 @@ const Note = ({openModal}) => {
   );
 };
   
-  export default Note;
+export default Note;
