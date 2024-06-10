a17d80504b1368ebb1fe3d031265480f51e0821e
JungKiHyun <jeongkyoni@gmail.com>
2024-06-11 06:56:50 +0900

    남은시간, 진행도 추가 노트 클릭시 정답, 오답 체크 추가

--- src/components/note/Note.js
+++ src/components/note/Note.js
@@ -6,12 +6,27 @@ const Note = ({openModal}) => {
   const [result, setResult] = useState('');
 
   const [notes, setNotes] = useState(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'G♯/A♭', 'D♯/E♭', 'A♯/B♭', 'F♯/G♭', 'C♯/D♭']);
+  const [noteAnswer, setNoteAnswer] = useState([['F', 'F♯/G♭', 'G', 'G♯/A♭', 'A', 'A♯/B♭', 'B', 'C', 'C♯/D♭', 'D', 'D♯/E♭', 'E'],
+                                                  ['C', 'C♯/D♭', 'D', 'D♯/E♭', 'E', 'F', 'F♯/G♭', 'G', 'G♯/A♭', 'A', 'A♯/B♭', 'B'],
+                                                  ['G♯/A♭', 'A', 'A♯/B♭', 'B', 'C', 'C♯/D♭', 'D', 'D♯/E♭', 'E', 'F', 'F♯/G♭', 'G'],
+                                                  ['D♯/E♭', 'E', 'F', 'F♯/G♭', 'G', 'G♯/A♭', 'A', 'A♯/B♭', 'B', 'C', 'C♯/D♭', 'D'],
+                                                  ['A♯/B♭', 'B', 'C', 'C♯/D♭', 'D', 'D♯/E♭', 'E', 'F', 'F♯/G♭', 'G', 'G♯/A♭', 'A'],
+                                                  ['F', 'F♯/G♭', 'G', 'G♯/A♭', 'A', 'A♯/B♭', 'B', 'C', 'C♯/D♭', 'D', 'D♯/E♭', 'E']])
   const guitarRef = useRef(null);
 
   const [fret, setFret] = useState(0)
   const [line, setLine] = useState(0)
-  const [count, setCount] = useState(1)
-  const [noteButtons, setNoteButtons] = useState()
+  const [count, setCount] = useState(0)
+  const [timeLeft, setTimeLeft] = useState(20);
+  const intervalRef = useRef();
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setTimeLeft(prevTimeLeft => Math.max(prevTimeLeft - 0.1, 0));
+    }, 100);
+
+    return () => clearInterval(intervalRef.current);
+  }, []);
 
   useEffect(() => {
     let fretRandom = Math.floor(Math.random() * 12) + 1
@@ -32,6 +47,16 @@ const Note = ({openModal}) => {
   }
 
   const handleNoteClick = (note) => {
+    console.log('f', fret)
+    console.log('l', line)
+    console.log(note)
+    console.log(noteAnswer[line-1][fret-1])
+    if(noteAnswer[line-1][fret-1] === note) {
+      alert('정답')
+    } else {
+      alert('오답')
+      return false;
+    }
     // setSelectedNote(note);
     // if (note === correctNote) {
     //   openModal('', '정답입니다!', () => {});
@@ -40,12 +65,25 @@ const Note = ({openModal}) => {
     // }
       // openModal('', '정답입니다!', () => {setCount(++count)});
     const addCount = count + 1
+    if(addCount > 10) {
+      alert('chrhk')
+      return false;
+    }
     setCount(addCount)
   };
 
   return (
     <div className="note-container">
-      {count}
+      <div className="timer-container">
+        <div className="timer-bar" style={{ width: `${(timeLeft / 20) * 100}%` }}>
+        </div>
+      </div>
+      <div className="text">남은시간</div>
+      <div className="progress-container">
+        <div className="progress-bar" style={{ width: `${count * 10}%` }}>
+        </div>
+      </div>
+      <div className="text">진행도</div>
       <div className="guitar-image" ref={guitarRef}>
         <img src={`/img/notes/note${fret}${line}.png`} alt="Guitar" />
       </div>
