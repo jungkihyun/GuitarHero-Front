0c3d3839c662ea349f284d493fa36adf12446e6e
JungKiHyun <jeongkyoni@gmail.com>
2024-08-03 00:46:24 +0900

    전체 레이아웃 변경중

--- src/components/noteGrid/NoteGrid.js
+++ src/components/noteGrid/NoteGrid.js
@@ -1,10 +1,67 @@
+import React from 'react';
 import './NoteGrid.scss';
 
-const NoteGrid = ({ openModal }) => {
+const NoteGrid = (props) => {
+  // 데이터 형태
+  const cells = Array(6).fill(null); // 길이 6의 배열 생성
+
   return (
-    <div className="note-wrapper">
-      <div className="note-container">
-        {/* Note content goes here */}
+    <div className="note-grid-container">
+      <div className="note-grid">
+        <div className="note-row">
+          {cells.map((_, index) => (
+            <div className={`note-cell ${props.startPlat === 1 && index === 0 ? 'note-cell-first' : ''}`} key={index}>
+              {parseInt(props.noteObj.line1[0]) === index
+              ? <span className={`target ${props.noteObj.line1[1] === 'X' ? 'target-x' : props.noteObj.line1[0] === '0' ? 'target-zero' : props.noteObj.line1[1] === ' ' ? 'target-note' : ''}`}>{props.noteObj.line1[1]}</span>
+              : <></>}
+            </div>
+          ))}
+        </div>
+        <div className="note-row">
+          {cells.map((_, index) => (
+            <div className={`note-cell ${props.startPlat === 1 && index === 0 ? 'note-cell-first' : ''}`} key={index}>
+              {parseInt(props.noteObj.line2[0]) === index
+              ? <span className={`target ${props.noteObj.line2[1] === 'X' ? 'target-x' : props.noteObj.line2[0] === '0' ? 'target-zero' : props.noteObj.line2[1] === ' ' ? 'target-note' : ''}`}>{props.noteObj.line2[1]}</span>
+              : <></>}
+            </div>
+          ))}
+        </div>
+        <div className="note-row">
+          {cells.map((_, index) => (
+            <div className={`note-cell ${props.startPlat === 1 && index === 0 ? 'note-cell-first' : ''}`} key={index}>
+              {parseInt(props.noteObj.line3[0]) === index
+              ? <span className={`target ${props.noteObj.line3[1] === 'X' ? 'target-x' : props.noteObj.line3[0] === '0' ? 'target-zero' : props.noteObj.line3[1] === ' ' ? 'target-note' : ''}`}>{props.noteObj.line3[1]}</span>
+              : <></>}
+            </div>
+          ))}
+        </div>
+        <div className="note-row">
+          {cells.map((_, index) => (
+            <div className={`note-cell ${props.startPlat === 1 && index === 0 ? 'note-cell-first' : ''}`} key={index}>
+              {parseInt(props.noteObj.line4[0]) === index
+              ? <span className={`target ${props.noteObj.line4[1] === 'X' ? 'target-x' : props.noteObj.line4[0] === '0' ? 'target-zero' : props.noteObj.line4[1] === ' ' ? 'target-note' : ''}`}>{props.noteObj.line4[1]}</span>
+              : <></>}
+            </div>
+          ))}
+        </div>
+        <div className="note-row">
+          {cells.map((_, index) => (
+            <div className={`note-cell ${props.startPlat === 1 && index === 0 ? 'note-cell-first' : ''}`} key={index}>
+              {parseInt(props.noteObj.line5[0]) === index
+              ? <span className={`target ${props.noteObj.line5[1] === 'X' ? 'target-x' : props.noteObj.line5[0] === '0' ? 'target-zero' : props.noteObj.line5[1] === ' ' ? 'target-note' : ''}`}>{props.noteObj.line5[1]}</span>
+              : <></>}
+            </div>
+          ))}
+        </div>
+        <div className="note-row">
+          {cells.map((_, index) => (
+            <div className={`note-cell ${props.startPlat === 1 && index === 0 ? 'note-cell-first' : ''}`} key={index}>
+              {parseInt(props.noteObj.line6[0]) === index
+              ? <><span className={`target ${props.noteObj.line6[1] === 'X' ? 'target-x' : props.noteObj.line6[0] === '0' ? 'target-zero' : props.noteObj.line6[1] === ' ' ? 'target-note' : ''}`}>{props.noteObj.line6[1]}</span>{index+props.startPlat}</>
+              : <>{index+props.startPlat}</>}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/page/note/Note2.js
+++ src/page/note/Note2.js
@@ -1,22 +1,55 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './Note2.scss';
+import NoteGrid from '../../components/noteGrid/NoteGrid';
 
-const Note2 = ({ openModal }) => {
+const Note2 = () => {
   const [selectedNote, setSelectedNote] = useState('');
   const [successResult, setSuccessResult] = useState(false);
   const [failResult, setFailResult] = useState(false);
 
+  const timeoutRef = useRef(null); // 타이머 ID를 저장할 ref
+
   const handleNoteClick = (note) => {
-    // 여기에 노트를 클릭했을 때의 로직을 추가하세요
-    if (note === 'C') {
+    // 기존 타이머가 있으면 제거
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    if (note === noteObj.correct) {
       setSuccessResult(true);
       setFailResult(false);
     } else {
       setSuccessResult(false);
       setFailResult(true);
     }
-    setSelectedNote(note);
+    
+    // 2초 뒤에 초기화
+    timeoutRef.current = setTimeout(() => {
+      setSuccessResult(false);
+      setFailResult(false);
+    }, 1500); // 2000ms = 2초
+    // setSelectedNote(note);
+
   };
+  
+  // const noteObj = {
+  //   line1: '0E',
+  //   line2: '1C',
+  //   line3: '0G',
+  //   line4: '2E',
+  //   line5: '3C',
+  //   line6: '0X',
+  // }
+  
+  const noteObj = {
+    line1: '0 ',
+    line2: '',
+    line3: '',
+    line4: '',
+    line5: '',
+    line6: '',
+    correct: 'E'
+  }
 
   return (
     <div className="note-wrapper">
@@ -30,8 +63,10 @@ const Note2 = ({ openModal }) => {
       </div>
       <div className="note-container">
         <div className="note-grid">
-          {/* NoteGrid 컴포넌트가 여기에 들어갑니다 */}
-          NoteGrid
+          <NoteGrid 
+            startPlat={1}
+            noteObj={noteObj}
+          />
         </div>
         <div className="note-buttons">
           <button onClick={() => handleNoteClick('C')}>C</button>
@@ -39,10 +74,6 @@ const Note2 = ({ openModal }) => {
           <button onClick={() => handleNoteClick('E')}>E</button>
           <button onClick={() => handleNoteClick('F')}>F</button>
           <button onClick={() => handleNoteClick('G')}>G</button>
-          <button onClick={() => handleNoteClick('A')}>A</button>
-          <button onClick={() => handleNoteClick('B')}>B</button>
-          <button onClick={() => handleNoteClick('A')}>A</button>
-          <button onClick={() => handleNoteClick('B')}>B</button>
           <button onClick={() => handleNoteClick('A')}>A</button>
           <button onClick={() => handleNoteClick('B')}>B</button>
         </div>
